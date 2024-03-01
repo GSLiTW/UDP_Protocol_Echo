707cfdb3fbc39f662dfbdc1ced631011c062b21b
GSLiTW <coslove8866@gmail.com>
2024-03-01 18:32:21 +0800

    Initial structure of sockets

---
+++ client.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char ** argv){
+    if(argc != 2){
+        printf("Should pass in a PORT number\n");
+        exit(0);
+    }
+
+    char *ip = "127.0.0.1";
+    int port = atoi(argv[1]);
+
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[1024];
+    socklen_t addr_size;
+    int n;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0){
+        perror("[-]socket error");
+        exit(1);
+    }
+
+    memset(&server_addr, '\0', sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    return 0;
+}
